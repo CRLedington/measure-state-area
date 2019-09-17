b915f5ab49a166ed7d83da61a64e1090b19f7957
Conner Reid Ledington <crle228@g.uky.edu>
2019-09-17 16:42:39 -0400

    measure polygon function
    for area

--- area.bat
+++ area.bat
@@ -1,12 +1,18 @@
+set data=c:\CRL-GIS\data\ne_10m_admin_1_states_provinces_lakes.shp
+set states='Texas','Alaska','Kentucky'
+set units=sq_mi
+set factor=2.59e+6
+set output=area.csv
+
 rem Create projected Shapefile
 ogr2ogr projected.shp -sql ^
- "select name from ne_10m_admin_1_states_provinces_lakes where name in ('Texas','Alaska','Kentucky')" ^
+ "select name from ne_10m_admin_1_states_provinces_lakes where name in (%states%)" ^
  -s_srs EPSG:4326 -t_srs EPSG:5070 ^
- c:\CRL-GIS\data\ne_10m_admin_1_states_provinces_lakes.shp
+ %data%
 
 rem Use ogr2ogr internal measurement feature
 ogr2ogr output.csv -sql ^
- "select name, (ogr_geom_area/2.59e+6) as sq_km from projected" ^
+ "select name, (ogr_geom_area/%factor%) as %units% from projected" ^
  projected.shp
 
 rem Delete intermediate files
